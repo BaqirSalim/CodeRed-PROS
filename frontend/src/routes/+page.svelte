587af363ef9fa6aad738a0e804a18a8650b70feb
Joshua Eworo <eworojoshua@gmail.com>
2024-02-10 18:59:00 -0600

    First frontend push boiissss

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<!--   
+    git config --global user.email "you@example.com"
+    git config --global user.name "Your Name" 
+-->
