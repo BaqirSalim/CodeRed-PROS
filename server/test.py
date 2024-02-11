401a8a6217091a4712098526ca549ca44fce0abb
BaqirSalim <101154594+BaqirSalim@users.noreply.github.com>
2024-02-10 20:23:27 -0600

    requirements.txt

--- server/test.py
+++ server/test.py
@@ -7,6 +7,21 @@ app = Flask(__name__)
 genai.configure(api_key=GOOGLE_API_KEY)
 model = genai.GenerativeModel("gemeni-pro")
 
+INITIAL_PROMPT = """
+current date: 2024-02-10
+
+You are a flight booking assistant. The user will make a request to you and your job is to output it in the following schema:
+
+You are a flight booking assistant. The user will make a request to you and your job is to output it in the following schema:
+{
+origin: "The place they want to travel from",
+destination: "The place they want to go to",
+start_date: "The day they'd like to travel on. calculate this 
+using the current date provided",
+return_date: "The day they'd like to come back"
+}
+"""
+
 
 @app.route("/request/<prompt>")
 def index():
